55220804a83ff642a2b49c6c08e3edb8526d138e
yosuke7040 <yosuke.bad4@gmail.com>
2025-12-08 13:18:49 +0900

    jj,jkの設定をnvimに

--- nvim/vscode_keymaps.lua
+++ nvim/vscode_keymaps.lua
@@ -17,9 +17,8 @@ vim.g.maplocalleader = " "
 -- インサートモードのキーバインド
 -- ===========================================
 -- jk で Esc
--- vscode側で設定しているためコメントアウト
--- vim.keymap.set('i', 'jk', '<Esc>', { noremap = true, silent = true })
--- vim.keymap.set('i', 'jj', '<Esc>', { noremap = true, silent = true })
+vim.keymap.set('i', 'jk', '<Esc>', { noremap = true, silent = true })
+vim.keymap.set('i', 'jk', '<Esc><Cmd>w<CR>', { noremap = true, silent = true }))
 
 -- ===========================================
 -- ノーマルモードのキーバインド
